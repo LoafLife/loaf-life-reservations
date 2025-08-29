767f9b9ea5720daaa7095a4d6e5e400bee376383
LoafLife <admin@loaf-life.com>
2025-08-29 15:34:59 -0400

    Add App.js main component wrapper

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoafLifeReservationSystem from './LoafLifeReservationSystem';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <LoafLifeReservationSystem />
+    </div>
+  );
+}
+
+export default App;
